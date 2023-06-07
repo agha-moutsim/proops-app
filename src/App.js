6b2212864cf1f9fb5b5698e39c0d2740e3cd44e9
aghamoutsim <aghamoutsim@gmail.com>
2023-06-07 11:37:25 +0500

    proops-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Proops from './Proops';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Proops car="lambo"/>
+      <Proops car="buggati"/>
+      <Proops car="feraari"/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Proops.jsx
@@ -0,0 +1,7 @@
+function Proops(Proops){
+    return(
+<div>my fav car is {Proops.car}</div>
+    );
+}
+
+export default Proops;
